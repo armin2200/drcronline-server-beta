3f0d53fbbae0baf0df60ac6446bf339b96bd15ca
Armin Zohrabi <armin.zohrabi@live.com>
2018-12-11 10:58:29 -0800

    make it better

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,33 +1,33 @@
-require("dotenv").load();
-var jwt = require("jsonwebtoken");
+require('dotenv').load();
+var jwt = require('jsonwebtoken');
 
 exports.loginRequired = function(req, res, next) {
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    const token = req.headers.authorization.split(' ')[1];
     jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
       if (decoded) {
         return next();
       } else {
-        return next({ status: 401, message: "Please Log In First" });
+        return next({ status: 401, message: 'Please Log In First' });
       }
     });
   } catch (e) {
-    return next({ status: 401, message: "Please Log In First" });
+    return next({ status: 401, message: 'Please Log In First' });
   }
 };
 
 exports.ensureCorrectUser = function(req, res, next) {
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    const token = req.headers.authorization.split(' ')[1];
     jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
       if (decoded && decoded._id === req.params.userId) {
         return next();
       } else {
-        return next({ status: 401, message: "Unauthorized" });
+        return next({ status: 401, message: 'Unauthorized' });
       }
     });
   } catch (e) {
-    return next({ status: 401, message: "Unauthorized1" });
+    return next({ status: 401, message: 'Unauthorized1' });
   }
 };
 
@@ -36,6 +36,6 @@ exports.isAdmin = function(req, res, next) {
   // 403 Forbidden
 
   if (!req.body.isAdmin)
-    return next({ status: 403, message: "Access denied." });
+    return next({ status: 403, message: 'Access denied.' });
   return next();
 };
